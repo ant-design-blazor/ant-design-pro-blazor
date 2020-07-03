38c57c3322f4afe36ac9fd6e401c1d8b8dae3fd8
zhaowenkai <799480165@qq.com>
2020-07-03 19:15:38 +0800

    add alain template style

---
+++ src/AntDesignPro/Pages/User/Lock.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntDesignPro
+{
+    public partial class Lock
+    {
+        private readonly User _user = new User();
+
+        public void Unlock()
+        {
+
+        }
+    }
+}
---
+++ src/AntDesignPro/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntDesignPro
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
