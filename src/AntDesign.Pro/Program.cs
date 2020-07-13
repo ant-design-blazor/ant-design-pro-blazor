0e6ae967e5e7fc1816af47b4b4ae539d609d94f9
chaldea <yoli799480165@gmail.com>
2020-07-13 14:20:58 +0800

    feat: initial project (#2)
    * add alain template style
    
    * add pro pages
    
    * [WIP] pro pages
    
    * clean project using
    
    * fix build error
    
    * refactor: directory structure
    
    * fix: wrong namespace
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ src/AntDesign.Pro/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntDesign.Pro
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            builder.Services.AddSingleton(x => new HttpClient());
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/AntDesign.Pro/gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require('gulp'),
+  cleanCss = require('gulp-clean-css'),
+  less = require('gulp-less'),
+  rename = require('gulp-rename'),
+  npmImport = require("less-plugin-npm-import");
+
+gulp.task('less', function () {
+  return gulp
+    .src('styles/index.less')
+    .pipe(less({
+      javascriptEnabled: true,
+      plugins: [new npmImport({ prefix: '~' })]
+    }))
+    .pipe(cleanCss({ compatibility: '*' }))
+    .pipe(rename('site.css'))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('default', gulp.parallel('less'), function () { })
