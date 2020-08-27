4ac380bfafab0d24a938f6e86633bf37ba6bc6f7
zhaowenkai <799480165@qq.com>
2020-08-27 21:10:26 +0800

    update page workplace

---
+++ src/AntDesign.Pro/Services/ProjectService.cs
@@ -0,0 +1,66 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AntDesign.Pro.Services
+{
+    public class NoticeType
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Logo { get; set; }
+        public string Description { get; set; }
+        public string UpdatedAt { get; set; }
+        public string Member { get; set; }
+        public string Href { get; set; }
+        public string MemberLink { get; set; }
+    }
+
+    public class ActivityUser
+    {
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+    }
+
+    public class ActivityGroup
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class ActivityProject
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class ActivitiesType
+    {
+        public string Id { get; set; }
+        public string UpdatedAt { get; set; }
+        public ActivityUser User { get; set; }
+        public ActivityGroup Group { get; set; }
+        public ActivityProject Project { get; set; }
+        public string Template { get; set; }
+    }
+
+    public class ProjectService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProjectService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<NoticeType[]> GetProjectNotice()
+        {
+            return await _httpClient.GetFromJsonAsync<NoticeType[]>("data/notice.json");
+        }
+
+        public async Task<ActivitiesType[]> GetActivities()
+        {
+            return await _httpClient.GetFromJsonAsync<ActivitiesType[]>("data/activities.json");
+        }
+    }
+}
