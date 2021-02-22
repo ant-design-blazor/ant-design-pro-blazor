2cd28674712594c707d0ea1345741cdfe6bacae9
Jed Buenaventura <dhyed.o04@gmail.com>
2021-02-23 00:31:34 +0800

    Translated menu and pages to english

--- src/AntDesign.Pro/Pages/User/Login/Login.razor.cs
+++ src/AntDesign.Pro/Pages/User/Login/Login.razor.cs
@@ -4,33 +4,28 @@ using AntDesign.Pro.Template.Services;
 using Microsoft.AspNetCore.Components;
 using AntDesign;
 
-namespace AntDesign.Pro.Template.Pages.User
-{
-    public partial class Login
-    {
-        private readonly LoginParamsType _model = new LoginParamsType();
+namespace AntDesign.Pro.Template.Pages.User {
+  public partial class Login {
+    private readonly LoginParamsType _model = new LoginParamsType();
 
-        [Inject] public NavigationManager NavigationManager { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
 
-        [Inject] public IAccountService AccountService { get; set; }
+    [Inject] public IAccountService AccountService { get; set; }
 
-        [Inject] public MessageService Message { get; set; }
+    [Inject] public MessageService Message { get; set; }
 
-        public void HandleSubmit()
-        {
-            if (_model.UserName == "admin" && _model.Password == "ant.design")
-            {
-                NavigationManager.NavigateTo("/");
-                return;
-            }
+    public void HandleSubmit() {
+      if (_model.UserName == "admin" && _model.Password == "ant.design") {
+        NavigationManager.NavigateTo("/");
+        return;
+      }
 
-            if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
-        }
-
-        public async Task GetCaptcha()
-        {
-            var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
-            await Message.Success($"获取验证码成功！验证码为：{captcha}");
-        }
+      if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
     }
+
+    public async Task GetCaptcha() {
+      var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
+      await Message.Success($"Verification code validated successfully! The verification code is: {captcha}");
+    }
+  }
 }
