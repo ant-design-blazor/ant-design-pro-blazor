28c177faa2ba1a2694bfa8813c35e5b012746d7d
James Yeung <shunjiey@hotmail.com>
2021-07-15 18:49:35 +0800

    update template projects to AntDesign 0.8.4 (#88)
    * update template projects to AntDesign 0.8.4
    
    * fix missing namespace
    
    * add FooterRender

--- src/AntDesign.Pro/Components/GlobalHeader/RightContent.razor.cs
+++ src/AntDesign.Pro/Components/GlobalHeader/RightContent.razor.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AntDesign;
 
 namespace AntDesign.Pro.Template.Components
 {
