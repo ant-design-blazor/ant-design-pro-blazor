a7f3c0ef1c54d6634a816543c373288e7a51d7fd
ElderJames <shunjiey@hotmail.com>
2024-01-22 00:52:19 +0000

    Deploying to gh-pages from  @ 948c6be3765bb7c405b96fa9517bbf71095d3081 🚀

---
+++ _content/AntDesign.ProLayout/AntDesign.ProLayout.lib.module.js
@@ -0,0 +1,36 @@
+﻿var beforeStartCalled = false;
+var afterStartedCalled = false;
+
+export function beforeWebStart() {
+    loadScriptAndStyle();
+}
+
+export function beforeStart(options, extensions) {
+   loadScriptAndStyle();
+}
+
+function loadScriptAndStyle() {
+    if (beforeStartCalled) {
+        return;
+    }
+
+    beforeStartCalled = true;
+
+    const antblazorCss = '_content/AntDesign/css/ant-design-blazor.css';
+    const prolayoutCss = '_content/AntDesign.ProLayout/css/ant-design-pro-layout-blazor.css';
+
+    if (!document.querySelector(`[href="${prolayoutCss}"]`)){
+        var customStyle = document.createElement('link');
+        customStyle.setAttribute('href', `${prolayoutCss}`);
+        customStyle.setAttribute('rel', 'stylesheet');
+
+        const cssMark = document.querySelector(`[href="${antblazorCss}"]`) || document.querySelector('[prolayout-css]') || document.querySelector('link');
+        if (cssMark) {
+            cssMark.after(customStyle);
+        }
+        else {
+            console.log('no antblazorStyle')
+            document.head.appendChild(customStyle);
+        }
+    }
+}
