1668a59d591f78c7ab45b609b66d0813721cae43
James Yeung <shunjiey@hotmail.com>
2024-06-18 00:18:39 +0800

    add webapp host to template

---
+++ .template.config/templates/webapp/AntDesign.Pro.Template/AntDesign.Pro.Template/Program.cs
@@ -0,0 +1,35 @@
+using AntDesign.Pro.Template.Client.Pages;
+using AntDesign.Pro.Template.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(AntDesign.Pro.Template.Client._Imports).Assembly);
+
+app.Run();
