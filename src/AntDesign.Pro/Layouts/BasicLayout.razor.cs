be8342e94bcdd8544f8b1ad9173b48d1867e7d51
Jaems Yeung <shunjiey@hotmail.com>
2024-08-23 15:51:21 +0800

    fix: build error without full pages

---
+++ src/AntDesign.Pro/Layouts/BasicLayout.razor.cs
@@ -0,0 +1,58 @@
+﻿using AntDesign.Extensions.Localization;
+using AntDesign.ProLayout;
+using Microsoft.AspNetCore.Components;
+using System.Globalization;
+using System.Net.Http.Json;
+
+namespace AntDesign.Pro.Template.Layouts
+{
+    public partial class BasicLayout : LayoutComponentBase, IDisposable
+    {
+        private MenuDataItem[] _menuData;
+
+        [Inject] private ReuseTabsService TabService { get; set; }
+        //#if (full)
+
+        [Inject] private HttpClient HttpClient { get; set; }
+
+        [Inject] private ILocalizationService LocalizationService { get; set; }
+
+        private EventHandler<CultureInfo> _localizationChanged;
+
+        //#endif
+
+        protected override async Task OnInitializedAsync()
+        {
+#if IsNotFull
+            _menuData = new[] {
+                new MenuDataItem
+                {
+                    Path = "/",
+                    Name = "welcome",
+                    Key = "welcome",
+                    Icon = "smile",
+                }
+            };
+#else
+            _localizationChanged = (sender, args) => InvokeAsync(StateHasChanged);
+            LocalizationService.LanguageChanged += _localizationChanged;
+            _menuData = await HttpClient.GetFromJsonAsync<MenuDataItem[]>("data/menu.json");
+#endif
+        }
+
+        void Reload()
+        {
+            TabService.ReloadPage();
+        }
+
+        public void Dispose()
+        {
+#if IsNotFull
+            
+#else
+            LocalizationService.LanguageChanged -= _localizationChanged;
+#endif
+        }
+
+    }
+}
